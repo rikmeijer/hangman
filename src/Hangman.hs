1ec45520c5f974bd4a3389baf9b88e8f304d739c
Rik Meijer <rik@pulledbits.org>
2018-09-12 15:56:20 +0200

    convert word to string of dots

--- src/Hangman.hs
+++ src/Hangman.hs
@@ -1,6 +1,7 @@
 module Hangman (hangman)  where
 
 import Hangman.IO
+import Data.List
 
 hangman :: IO ()
 hangman = do
@@ -8,6 +9,7 @@ hangman = do
     namePlayer1 <- ask (questionPlayerName "1")
     namePlayer2 <- ask (questionPlayerName "2")
     putStrLn (draw namePlayer1 namePlayer2)
+    play "Feesttent"
 
 questionPlayerName :: String -> String
 questionPlayerName index = "Please enter the name of player " ++ index ++ " (use 'C' for computer): "
@@ -16,3 +18,7 @@ draw :: String -> String -> String
 draw player1 player2 = "The draw was made: " ++ startingPlayer player1 ++ " begins."
   where startingPlayer "C" = "Computer"
         startingPlayer player1 = player1
+
+play :: String -> IO ()
+play word = do
+  putStrLn $ map (\a -> '.') word
