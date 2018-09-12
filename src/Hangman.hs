ce43f15610eb6ce30bdb032ce0d8fd8f94dee13e
Rik Meijer <rik@pulledbits.org>
2018-09-12 16:15:22 +0200

    ask for a guess and store in guesses

--- src/Hangman.hs
+++ src/Hangman.hs
@@ -9,7 +9,7 @@ hangman = do
     namePlayer1 <- ask (questionPlayerName "1")
     namePlayer2 <- ask (questionPlayerName "2")
     putStrLn (draw namePlayer1 namePlayer2)
-    play "Feesttent"
+    play "" "Feesttent"
 
 questionPlayerName :: String -> String
 questionPlayerName index = "Please enter the name of player " ++ index ++ " (use 'C' for computer): "
@@ -19,6 +19,16 @@ draw player1 player2 = "The draw was made: " ++ startingPlayer player1 ++ " begi
   where startingPlayer "C" = "Computer"
         startingPlayer player1 = player1
 
-play :: String -> IO ()
-play word = do
+play :: String -> String -> IO ()
+play guesses word = do
   putStrLn $ map (\a -> '.') word
+  putStrLn ("Errors: 0 (" ++ guesses ++ ")")
+  putStrLn (generateGallows 0)
+  characterGuess <- ask "Enter your guess: "
+  play (guesses ++ characterGuess) word
+
+
+generateGallows :: Int -> String
+generateGallows state
+  | state == 0 = "|__"
+  | otherwise = "unknown"
