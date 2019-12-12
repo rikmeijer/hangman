dd754cbc3cf5fc472a4b0eb0c19b82eb3837fda1
Rik Meijer <rik@pulledbits.org>
2019-12-12 14:35:19 +0100

    also test asking name for player 2

--- test/Hangman/PlayerTests.hs
+++ test/Hangman/PlayerTests.hs
@@ -5,11 +5,12 @@ module Hangman.PlayerTests
 import           Hangman.Player
 import           Test.HUnit
 
-testAskName index = TestCase (assertEqual
-                             "should return question for player name"
-                             ("Please enter the name of player " ++ index ++ " (use 'C' for computer): ")
-                             (Hangman.Player.askName index))
+testAskName index =
+  TestCase
+    (assertEqual
+       "should return question for player name"
+       ("Please enter the name of player " ++ index ++ " (use 'C' for computer): ")
+       (Hangman.Player.askName index))
 
 tests =
-  TestList
-    [ TestLabel "ask user '1' for name" (testAskName "1")]
+  TestList [TestLabel "ask user '1' for name" (testAskName "1"), TestLabel "ask user '2' for name" (testAskName "2")]
